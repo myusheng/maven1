841494b900c7fc65702ae8a9cd3cadbd8faae53e
“mengy <271328795@qq.com>
2020-01-30 11:55:33 +0800

    第一次提交

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("mengyu");
+        System.out.println("mengyu");
+        System.out.println("mengyu");
+    }
+}
